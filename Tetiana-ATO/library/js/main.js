2f18c4de7c494eb68760ea5aa4ffe6d4c0962e0f
Tetiana <atoit.2019@gmail.com>
2021-10-18 19:33:57 +0300

    new button-top

--- Tetiana-ATO/library/js/main.js
+++ Tetiana-ATO/library/js/main.js
@@ -4,5 +4,17 @@ $(function(){
     $('.menu__list').toggleClass('menu__list--active');
   });
 
+  // $(window).scroll(function(){
+  //   if ($(this).scrollTop() > 100) {
+  //   $('.scrollup').fadeIn();
+  //   } else {
+  //   $('.scrollup').fadeOut();
+  //   }
+  // });
+  
+  // $('.scrollup').click(function(){
+  //   $("html, body").animate({ scrollTop: 0 }, 600);
+  //   return false;
+  // });
 
 });
